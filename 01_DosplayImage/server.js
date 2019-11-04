525410adbd3ce6b28acda87db5d369e7c6b547ed
Tsai <czn826@gmail.com>
2019-11-04 14:32:51 +1100

    Multiple Search
    Add multiple search

--- 01_DosplayImage/server.js
+++ 01_DosplayImage/server.js
@@ -10,19 +10,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {result: null, error: null});
+  res.render('index', {result: null, error: null, mutipleResult: null});
 })
 
 app.post('/', function (req, res) {
   let number = req.body.number;
+  let first_number = req.body.first_number;
+  let last_number = req.body.last_number;
   //let url = `data/all.json`
   let url = `https://api.spacexdata.com/v3/launches`
 
   request(url, function (err, response, body) {
     let json = JSON.parse(body)
-    let img = `${json[number].links.mission_patch}`;
-    let OutputText = `The ${number} flight of SpaceX is ${json[number].mission_name} in ${json[number].launch_date_utc}!`;
-        res.render('index', {result: OutputText, error: null, image: img});
+
+    var multipleText = [];
+    var OutputText;
+    var img;
+    var multiimage = [];
+
+    var inputValue = req.body.vote;
+
+    if (inputValue == "single"){
+      img = `${json[number].links.mission_patch}`;
+      OutputText = `The ${number} flight of SpaceX is ${json[number].mission_name} in ${json[number].launch_date_utc}!`;
+    }
+
+    if (inputValue == "multiple"){
+      var multext;
+      for (i = first_number; i<= last_number; i++){
+        multext = `The ${i} flight of SpaceX is ${json[i].mission_name} in ${json[i].launch_date_utc}!`;
+        multipleText.push(multext);
+      }
+      var mulimg;
+      for (i = first_number; i<= last_number; i++){
+        mulimg = `${json[i].links.mission_patch}`;
+        multiimage.push(mulimg);
+      }
+    }
+
+    
+    res.render('index', {result: OutputText, error: null, image: img, mutipleResult: multipleText, multiimage: multiimage});
+ 
   });
 
   // request(url, function (err, response, body) {
@@ -39,6 +67,8 @@ app.post('/', function (req, res) {
   //   }
   // });
 })
+
+
 
 app.listen(9000, function () {
   console.log('Example app listening on port 9000!')
