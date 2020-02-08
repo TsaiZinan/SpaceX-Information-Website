a05f769762d1c3af0d0081710496669cafbeca75
Tsai <czn826@gmail.com>
2020-02-09 04:52:30 +1100

    Vue_Initial_BasicDisplay

---
+++ 02_byvue/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><b>SpaceX Information Display</b></b-nav-item>
+      <b-nav-item>About</b-nav-item>
+    </b-nav>
+  </div>
+</template>
